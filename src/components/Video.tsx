6c2dd1be223749ec1b47f26849e9bca5f53e453d
Eduardy <eduardylopes@gmail.com>
2022-06-24 21:45:20 -0300

    implement video player and slug

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,4 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import {
   CaretRight,
   DiscordLogo,
@@ -6,38 +7,88 @@ import {
   Lightning,
 } from "phosphor-react";
 
-export function Video() {
+import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    },
+    fetchPolicy: "no-cache",
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <span>Carregando...</span>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video bg-purple-600"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video bg-purple-600">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
       <div className="p-8 max-w-[1100px] m-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold ">
-              Aula 01 - Abertura do Ignite Lab
-            </h1>
+            <h1 className="text-2xl font-bold ">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Nessa aula vamos dar início ao projeto criando a estrutura base da
-              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
-              realizar o setup do nosso projeto no GraphCMS criando as entidades
-              da aplicação e integrando a API GraphQL gerada pela plataforma no
-              nosso front-end utilizando Apollo Client.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/eduardylopes.png"
+                src={data.lesson.teacher.avatarURL}
                 alt=""
               />
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Eduardy Lopes
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Aspirante a professor
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -4,3 +4,22 @@ export const client = new ApolloClient({
   uri: "https://api-sa-east-1.graphcms.com/v2/cl4onj8870s4o01w7a3t6fltn/master",
   cache: new InMemoryCache(),
 });
+
+// import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+// const defaultOptions: DefaultOptions = {
+//   watchQuery: {
+//     fetchPolicy: "no-cache",
+//     errorPolicy: "ignore",
+//   },
+//   query: {
+//     fetchPolicy: "no-cache",
+//     errorPolicy: "all",
+//   },
+// };
+
+// export const client = new ApolloClient({
+//   uri: "https://api-sa-east-1.graphcms.com/v2/cl4onj8870s4o01w7a3t6fltn/master",
+//   cache: new InMemoryCache(),
+//   defaultOptions: defaultOptions,
+// });
