f87bd4e0bcfed104b90a16e674f97597900b426e
Eduardy <eduardylopes@gmail.com>
2022-06-25 13:42:25 -0300

    create subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,25 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4onj8870s4o01w7a3t6fltn/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
-
-// import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
-
-// const defaultOptions: DefaultOptions = {
-//   watchQuery: {
-//     fetchPolicy: "no-cache",
-//     errorPolicy: "ignore",
-//   },
-//   query: {
-//     fetchPolicy: "no-cache",
-//     errorPolicy: "all",
-//   },
-// };
-
-// export const client = new ApolloClient({
-//   uri: "https://api-sa-east-1.graphcms.com/v2/cl4onj8870s4o01w7a3t6fltn/master",
-//   cache: new InMemoryCache(),
-//   defaultOptions: defaultOptions,
-// });
